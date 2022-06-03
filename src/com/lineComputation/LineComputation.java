c34f3a8bf177a54306188a8c0058104fc527a146
MANOJ-51 <m4anoj51@gmail.com>
2022-06-03 16:38:52 +0530

    COMMITED: master branch

---
+++ src/com/lineComputation/LineComputation.java
@@ -0,0 +1,9 @@
+package com.lineComputation;
+
+import java.util.Scanner;
+
+public class LineComputation {
+    public static void main(String[] args) {
+        System.out.println("WELCOME TO LINE COMPARISON PROGRAM ");
+    }
+}
